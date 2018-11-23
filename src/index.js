95e51313fd5fc7847f5d510c7408870fc578ec82
Renisha Christie. A <christie.reni@gmail.com>
2018-11-23 21:11:49 +0530

    Initialize fontawesome icons

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faLinkedinIn, faGithub, faGithubAlt } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faCircle } from '@fortawesome/free-solid-svg-icons';
+import { faDotCircle } from '@fortawesome/free-regular-svg-icons';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+
+library.add(faLinkedinIn, faGithub, faGithubAlt, faEnvelope, faCircle, faDotCircle);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
