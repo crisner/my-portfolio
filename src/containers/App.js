af3b996572d91bac3d61e2fb6d16e2f95b164dcc
Renisha Christie. A <christie.reni@gmail.com>
2018-11-27 14:55:36 +0530

    Remove logo

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Home from '../components/Home';
 import About from '../components/About';
 import Contact from '../components/Contact';
-// import logo from '../images/logo.svg';
 import './App.css';
 
 class App extends Component {
