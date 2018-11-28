50558773c5e5048bb2c0ee3f1eaf396f9ba32ef7
crisner <christie.reni@gmail.com>
2018-11-28 20:14:19 +0530

    Add enclosing div and add image width for logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,16 +5,17 @@ import Menu from './Menu';
 
 function Navigation() {
     return (
-        <header>
-            <img src={logo} alt="logo" />
-            <ul>
-                <li><FontAwesomeIcon icon="envelope" /></li>
-                <li><FontAwesomeIcon icon={['fab', 'linkedin-in']} /></li>
-                <li><FontAwesomeIcon icon={['fab', 'github']} /></li>
-            </ul>
-        </header>
-        <Menu />
-
+        <div>
+            <header>
+                <img src={logo} alt="logo" width="60px" />
+                <ul>
+                    <li><FontAwesomeIcon icon="envelope" /></li>
+                    <li><FontAwesomeIcon icon={['fab', 'linkedin-in']} /></li>
+                    <li><FontAwesomeIcon icon={['fab', 'github']} /></li>
+                </ul>
+            </header>
+            <Menu />
+        </div>
     );
 }
 
