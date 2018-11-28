30a7b24828a3e8b29b30a9878c1c98449aa92801
crisner <christie.reni@gmail.com>
2018-11-28 21:20:35 +0530

    Import Portfolio component

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import Home from '../components/Home';
 import About from '../components/About';
 import Contact from '../components/Contact';
 import Navigation from '../components/Navigation';
+import Portfolio from './Portfolio';
 import './App.css';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
       <div className="App">
         <Navigation />
         <Home />
+        <Portfolio />
         <About />
         <Contact />
       </div>
