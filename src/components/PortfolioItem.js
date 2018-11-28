3ddf8a1e99698eb54c38795776dc3f6f06772420
crisner <christie.reni@gmail.com>
2018-11-28 21:19:54 +0530

    Create portfolio item component

---
+++ src/components/PortfolioItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Pill from './Pill';
+
+function PortfolioItem(props) {
+    return (
+        <div>
+            <div>
+                <img src={props.thumbnail} width="100px" alt={props.title} />
+                <a href={props.repo}><FontAwesomeIcon icon={['fab', 'github-alt']} /> Go to github repo</a>
+                <a href={props.live}><FontAwesomeIcon icon={['fab', 'github-alt']} /> View live</a>
+            </div>
+            <div>
+                <h4>{props.title}</h4>
+                <p>{props.description}</p>
+                {
+                    props.skills.map(skill => {
+                        return (
+                            <Pill text={skill} />
+                        );
+                    })
+                }
+            </div>
+        </div>
+    );
+}
+
+export default PortfolioItem;
