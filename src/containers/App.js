9f915fb9f9af9ee99f8b56ce34aba751347cc45a
crisner <christie.reni@gmail.com>
2018-11-28 20:14:39 +0530

    Import navigation component

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import Home from '../components/Home';
 import About from '../components/About';
 import Contact from '../components/Contact';
+import Navigation from '../components/Navigation';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navigation />
         <Home />
         <About />
         <Contact />
