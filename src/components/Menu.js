c544cee61a498a67342abc57daa25da797b42bf6
crisner <christie.reni@gmail.com>
2018-11-29 21:22:54 +0530

    Add styles to menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './Menu.module.css';
 
 function Menu() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <FontAwesomeIcon icon="circle" />
+        <nav className={styles.nav}>
+            <ul className={styles.menu}>
+                <li className={styles.hoverLink}>
+                    <FontAwesomeIcon className={styles.menuBullet} icon="circle" />
                     {/* <svg height="200" width="200">
                         <circle class="circle" cx="100" cy="100" r="95" stroke="#231f20" stroke-width="10" fill-opacity="0" />
                     </svg> */}
-                    Home
+                    <a href="#">Home</a>
                 </li>
-                <li><FontAwesomeIcon icon="circle" /></li>
-                <li><FontAwesomeIcon icon="circle" /></li>
-                <li>
-                    <FontAwesomeIcon icon="circle" />
+                <li><FontAwesomeIcon className={styles.bullet} icon="circle" /></li>
+                <li><FontAwesomeIcon  className={styles.bullet} icon="circle" /></li>
+                <li className={styles.hoverLink}>
+                    <FontAwesomeIcon className={styles.menuBullet} icon="circle" />
                     {/* <svg height="200" width="200">
                         <circle class="circle" cx="100" cy="100" r="95" stroke="#231f20" stroke-width="10" fill-opacity="0" />
                     </svg> */}
-                    Portfolio
+                    <a href="#">Portfolio</a>
                 </li>
-                <li><FontAwesomeIcon icon="circle" /></li>
-                <li><FontAwesomeIcon icon="circle" /></li>
-                <li><FontAwesomeIcon icon="circle" />About me</li>
-                <li><FontAwesomeIcon icon="circle" /></li>
-                <li><FontAwesomeIcon icon="circle" /></li>
-                <li><FontAwesomeIcon icon="circle" />Contact</li>
+                <li><FontAwesomeIcon  className={styles.bullet} icon="circle" /></li>
+                <li><FontAwesomeIcon  className={styles.bullet} icon="circle" /></li>
+                <li className={styles.hoverLink}><FontAwesomeIcon className={styles.menuBullet} icon="circle" /><a href="#">About me</a></li>
+                <li><FontAwesomeIcon  className={styles.bullet} icon="circle" /></li>
+                <li><FontAwesomeIcon  className={styles.bullet} icon="circle" /></li>
+                <li className={styles.hoverLink}><FontAwesomeIcon className={styles.menuBullet} icon="circle" /><a href="#">Contact</a></li>
             </ul>
         </nav>
     );
