40f17c79eead17c5ef5f5c4fcbbfccffa448f5da
crisner <christie.reni@gmail.com>
2018-11-29 21:22:34 +0530

    Add styles to header

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import logo from '../images/logo.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './Navigation.module.css';
 import Menu from './Menu';
 
 function Navigation() {
     return (
         <div>
             <header>
-                <img src={logo} alt="logo" width="60px" />
-                <ul>
-                    <li><FontAwesomeIcon icon="envelope" /></li>
-                    <li><FontAwesomeIcon icon={['fab', 'linkedin-in']} /></li>
-                    <li><FontAwesomeIcon icon={['fab', 'github']} /></li>
+                <img className={styles.logo} src={logo} alt="logo" />
+                <ul className={styles.social}>
+                    <li className={styles.socialIcons}><a href='#'><FontAwesomeIcon icon="envelope" /></a></li>
+                    <li className={styles.socialIcons}><a href='#'><FontAwesomeIcon icon={['fab', 'linkedin-in']} /></a></li>
+                    <li className={styles.socialIcons}><a href='#'><FontAwesomeIcon icon={['fab', 'github']} /></a></li>
                 </ul>
             </header>
             <Menu />
