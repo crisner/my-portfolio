9c0828b49bb5b29aa08e5d15d72f36d5c6414985
crisner <christie.reni@gmail.com>
2018-12-06 20:46:10 +0530

    Add a wrapper around buttons

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Pill from './Pill';
+import styles from './About.module.css';
 
 function About() {
     return (
-        <div>
+        <div className={styles.position}>
             <h3>About me</h3>
             <p>I am a self-taught front-end web developer with a background in the field of electronic
             media. I like learning new things and I admire art in all its forms but I enjoy creative paintings
@@ -11,7 +12,10 @@ function About() {
             <p>I am fascinated by languages and love finding similarities between them and learning
             them. This is also why got into coding. I find happiness in building something
             interactive with code.</p>
-            <Pill text="Resume" />
+            <div className={styles.button}>
+                <Pill title="Download my resume" text="Resume" />
+                <Pill title="Read my blog" text="Blog" />
+            </div>
         </div>
     );
 }
