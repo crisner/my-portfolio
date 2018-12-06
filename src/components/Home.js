756cc16252ee5ee616d2caaa61301f8f41a2704f
crisner <christie.reni@gmail.com>
2018-12-06 19:49:09 +0530

    Style home section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './Home.module.css';
 
 function Home() {
     return (
-        <div>
+        <div className={styles.Home}>
             <h1>Renisha Christie</h1>
             <h2>Front End Developer</h2>
         </div>
