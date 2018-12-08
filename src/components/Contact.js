e7adc6daef728fb4198ebc1738c78495742f293b
crisner <christie.reni@gmail.com>
2018-12-08 20:37:55 +0530

    Style contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './Contact.module.css';
 
 function Contact() {
     return (
-        <div>
+        <div className={styles.position}>
             <h3>Get in touch</h3>
-            <ul>
-                <li><FontAwesomeIcon icon="envelope" />Send an email</li>
-                <li><FontAwesomeIcon icon={['fab', 'linkedin-in']} />Connect with me</li>
-                <li><FontAwesomeIcon icon={['fab', 'github']} />Find me on Github</li>
+            <ul className={styles.contact}>
+                <li className={styles.modes}><a href="/"><FontAwesomeIcon className={styles.icons} icon="envelope" /><span className={styles.text}>Send an email</span></a></li>
+                <li className={styles.modes}><a href="/"><FontAwesomeIcon className={styles.icons} icon={['fab', 'linkedin-in']} /><span className={styles.text}>Connect with me</span></a></li>
+                <li className={styles.modes}><a href="/"><FontAwesomeIcon className={styles.icons} icon={['fab', 'github']} /><span className={styles.text}>Find me on Github</span></a></li>
             </ul>
         </div>
     );
