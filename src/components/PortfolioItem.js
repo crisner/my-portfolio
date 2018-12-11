b7ae14f14236ca3ee4ddca8871e3463fe2ed491a
crisner <christie.reni@gmail.com>
2018-12-11 20:05:54 +0530

    Style portfolio item component

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -9,23 +9,25 @@ function PortfolioItem(props) {
             <div className={styles.thumbnail}>
                 <div className={styles.overlay}>
                     <div className={styles.links}>
-                        <a href={props.repo}><span className={styles.circle}><FontAwesomeIcon icon={['fab', 'github-alt']} /></span> Go to github repo</a>
+                        <a href={props.repo}><span className={styles.circle}><FontAwesomeIcon icon={['fab', 'github-alt']} /></span> Go to repo</a>
                         <a href={props.live}><span className={styles.circle}><FontAwesomeIcon icon={['fab', 'github-alt']} /></span> View live</a>
                     </div>
                 </div>
                 <img src={props.thumbnail} alt={props.title} />
                 
             </div>
-            <div>
+            <div className={styles.details}>
                 <h4>{props.title}</h4>
                 <p>{props.description}</p>
+                <div className={styles.skills}>
                 {
                     props.skills.map(skill => {
                         return (
-                            <Pill key={skill} text={skill} />
+                            <Pill styles={styles.pill} key={skill} text={skill} />
                         );
                     })
                 }
+                </div>
             </div>
         </div>
     );
