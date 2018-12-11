43ac08b8b6624cd6c19d48a36828020377317ede
crisner <christie.reni@gmail.com>
2018-12-11 21:15:06 +0530

    Add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+function Footer() {
+    return (
+        <footer className={styles.Footer}>
+            &#xa9; 2018 - Renisha Christie. A
+        </footer>
+    );
+}
+
+export default Footer;
