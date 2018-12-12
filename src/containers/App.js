8c38f4a56374c43e5349846983dfb2269da0636f
crisner <christie.reni@gmail.com>
2018-12-12 20:53:59 +0530

    Add routes and use switch for rendering

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Home from '../components/Home';
 import About from '../components/About';
 import Contact from '../components/Contact';
 import Navigation from '../components/Navigation';
 import Portfolio from './Portfolio';
+import Footer from '../components/Footer';
 import './App.css';
 
 class App extends Component {
@@ -11,10 +13,14 @@ class App extends Component {
     return (
       <div className="App">
         <Navigation />
-        <Home />
-        <Portfolio />
-        <About />
-        <Contact />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route component={Home} />
+        </Switch>
+        <Footer />
       </div>
     );
   }
