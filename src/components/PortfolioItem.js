9d2cc4e634c8143b7d15e54844f64a02435e98bb
crisner <christie.reni@gmail.com>
2018-12-19 18:17:06 +0530

    Change to stateful component and add conditional rendering for links

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,36 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Pill from './Pill';
 import styles from './PortfolioItem.module.css';
 
-function PortfolioItem(props) {
-    return (
-        <div className={styles.Item}>
-            <div className={styles.thumbnail}>
-                <div className={styles.overlay}>
-                    <div className={styles.links}>
-                        <a href={props.repo}><span className={styles.circle}><FontAwesomeIcon icon={['fab', 'github-alt']} /></span> Go to repo</a>
-                        <a href={props.live}><span className={styles.circle}><FontAwesomeIcon icon="eye" /></span> View live</a>
+// let onHover = () => {
+//     console.log('hovered');
+// }
+
+class PortfolioItem extends Component {
+    state = {
+        hover: false
+    }
+    
+    onHover = () => {
+        this.setState({ hover: true });
+        
+    }
+    onLeave = () => {
+        this.setState({ hover: false });
+    }
+    render() {
+        console.log(this.state.hover);
+        return (
+            <div className={styles.Item}>
+                <div className={styles.thumbnail}>
+                    <div onMouseEnter={this.onHover} onMouseLeave={this.onLeave} className={styles.overlay}>
+                        <div className={styles.links}>
+                            <a href={this.props.repo} className={this.state.hover ? styles.top + ' ' + styles.topOnHover : styles.top}><span className={styles.circle}><FontAwesomeIcon icon={['fab', 'github-alt']} /></span> Go to repo</a>
+                            <a href={this.props.live} className={this.state.hover ? styles.bottom + ' ' + styles.bottomOnHover : styles.bottom}><span className={styles.circle}><FontAwesomeIcon icon="eye" /></span> View live</a>
+                        </div>
+                    </div>
+                    <img src={this.props.thumbnail} alt={this.props.title} />
+                    
+                </div>
+                <div className={styles.details}>
+                    <h4>{this.props.title}</h4>
+                    <p>{this.props.description}</p>
+                    <div className={styles.skills}>
+                    {
+                        this.props.skills.map(skill => {
+                            return (
+                                <Pill styles={styles.pill} key={skill} text={skill} />
+                            );
+                        })
+                    }
                     </div>
                 </div>
-                <img src={props.thumbnail} alt={props.title} />
-                
             </div>
-            <div className={styles.details}>
-                <h4>{props.title}</h4>
-                <p>{props.description}</p>
-                <div className={styles.skills}>
-                {
-                    props.skills.map(skill => {
-                        return (
-                            <Pill styles={styles.pill} key={skill} text={skill} />
-                        );
-                    })
-                }
-                </div>
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default PortfolioItem;
