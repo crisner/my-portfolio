3afa153f4327b4fcab83d03a0c3f25bffdfaf398
crisner <christie.reni@gmail.com>
2018-12-20 11:16:33 +0530

    Add mobile menu component

---
+++ src/components/MobileMenu.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './MobileMenu.module.css';
+
+function MobileMenu(props) {
+    // const {location} = props.location;
+    return (
+        <nav className={styles.nav}>
+            <ul className={styles.menu}>
+                <li onClick={props.expandMenu}>
+                    <NavLink exact to="/" className={styles.hoverLink}
+                    activeStyle={{
+                        background: "#ee4848",
+                        boxShadow: "1px 1px 2px rgba(206, 62, 62,1)"
+                    }}>
+                        Home
+                    </NavLink>
+                </li>
+                
+                <li onClick={props.expandMenu}>
+                    <NavLink to="/portfolio" className={styles.hoverLink}
+                    activeStyle={{
+                        background: "#ee4848",
+                        boxShadow: "1px 1px 2px rgba(206, 62, 62,1)"
+                    }}>
+                        Portfolio
+                    </NavLink>
+                </li>
+                
+                <li onClick={props.expandMenu}>
+                    <NavLink to="/about" className={styles.hoverLink} 
+                    activeStyle={{
+                        background: "#ee4848",
+                        boxShadow: "1px 1px 2px rgba(206, 62, 62,1)"
+                    }}>
+                        About me
+                    </NavLink>
+                </li>
+                
+                <li onClick={props.expandMenu}>
+                    <NavLink to="/contact" className={styles.hoverLink} 
+                    activeStyle={{
+                        background: "#ee4848",
+                        boxShadow: "1px 1px 2px rgba(206, 62, 62,1)"
+                    }}>
+                        Contact
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default MobileMenu;
