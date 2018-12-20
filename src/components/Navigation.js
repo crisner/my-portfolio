30d3b3a828a475d1679d619bb94c8d5460b93653
crisner <christie.reni@gmail.com>
2018-12-20 11:19:44 +0530

    Add hamburger menu functionality for small devices

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,40 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../images/logo.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Navigation.module.css';
 import Menu from './Menu';
+import MobileMenu from './MobileMenu';
 
-function Navigation(props) {
-    return (
-        <div>
-            <header>
-                <img className={styles.logo} src={logo} alt="logo" />
-                <div className={styles.hamburgerIcon}>
-                    <span className={styles.line}></span>
-                    <span className={styles.line}></span>
-                    <span className={styles.line}></span>
-                </div>
-                <ul className={styles.social}>
-                    <li className={styles.socialIcons}>
-                        <a href="mailto:christie.reni@gmail.com">
-                            <FontAwesomeIcon icon="envelope" />
-                        </a>
-                    </li>
-                    <li className={styles.socialIcons}>
-                        <a rel="noopener noreferrer" href="https://www.linkedin.com/in/renishachristie/" target="_blank">
-                            <FontAwesomeIcon icon={['fab', 'linkedin-in']} />
-                        </a>
-                    </li>
-                    <li className={styles.socialIcons}>
-                        <a rel="noopener noreferrer" href="https://github.com/crisner" target="_blank">
-                            <FontAwesomeIcon icon={['fab', 'github']} />
-                        </a>
-                    </li>
-                </ul>
-            </header>
-            <Menu location={props.location} />
-        </div>
-    );
+class Navigation extends Component {
+    state = {
+        open: false
+    }
+
+    expandMenu = () => {
+        this.setState({ open: !this.state.open })
+    }
+    render() {
+        console.log(this.state.open);
+        return (
+            <div>
+                <header>
+                    <img className={styles.logo} src={logo} alt="logo" />
+                    <div className={styles.hamburgerIcon} 
+                    onClick={this.expandMenu}>
+                        <span className={styles.line}></span>
+                        <span className={styles.line}></span>
+                        <span className={styles.line}></span>
+                    </div>
+                    <ul className={styles.social}>
+                        <li className={styles.socialIcons}>
+                            <a href="mailto:christie.reni@gmail.com">
+                                <FontAwesomeIcon icon="envelope" />
+                            </a>
+                        </li>
+                        <li className={styles.socialIcons}>
+                            <a rel="noopener noreferrer" href="https://www.linkedin.com/in/renishachristie/" target="_blank">
+                                <FontAwesomeIcon icon={['fab', 'linkedin-in']} />
+                            </a>
+                        </li>
+                        <li className={styles.socialIcons}>
+                            <a rel="noopener noreferrer" href="https://github.com/crisner" target="_blank">
+                                <FontAwesomeIcon icon={['fab', 'github']} />
+                            </a>
+                        </li>
+                    </ul>
+                </header>
+                {
+                    this.state.open ? <MobileMenu location={this.props.location} expandMenu={this.expandMenu.bind(this)} /> : null
+                }
+                <Menu location={this.props.location} />
+            </div>
+        );
+    }
 }
 
 export default Navigation;
