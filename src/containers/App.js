472ce0109865f09f3fdc4c4909602ce96cbde6b0
crisner <christie.reni@gmail.com>
2018-12-25 19:11:08 +0530

    Add fade css transition

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 import Home from '../components/Home';
 import About from '../components/About';
 import Contact from '../components/Contact';
@@ -12,19 +13,37 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route render={(props) => {
-          return (
-            <Navigation location={props} />
-          )
-        }} />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/portfolio" component={Portfolio} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route component={Home} />
-        </Switch>
-        <Footer />
+      <CSSTransition
+        in={true}
+        appear={true}
+        timeout={600}
+        classNames="fade">
+          <Route render={(props) => {
+            return (
+              <Navigation location={props} />
+            )
+          }} />
+        </CSSTransition>
+        <CSSTransition
+        in={true}
+        appear={true}
+        timeout={600}
+        classNames="fade">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={Home} />
+          </Switch>
+        </CSSTransition>
+        <CSSTransition
+        in={true}
+        appear={true}
+        timeout={600}
+        classNames="fade">
+          <Footer />
+        </CSSTransition>
       </div>
     );
   }
