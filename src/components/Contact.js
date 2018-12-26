7d368cf2094c9a966a14eb2df2cd17a4df81eb16
crisner <christie.reni@gmail.com>
2018-12-26 18:51:00 +0530

    Add state based hover styles to icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,37 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Contact.module.css';
 
-function Contact() {
-    return (
-        <div className={styles.position}>
-            <h3>Get in touch</h3>
-            <ul className={styles.contact}>
-                <li className={styles.modes}>
-                    <a href="mailto:christie.reni@gmail.com">
-                        <FontAwesomeIcon className={styles.icons} icon="envelope" />
-                        <span className={styles.text}>Send an email</span>
-                    </a>
-                </li>
-                <li className={styles.modes}>
-                    <a rel="noopener noreferrer" 
-                    href="https://www.linkedin.com/in/renishachristie/" 
-                    target="_blank">
-                        <FontAwesomeIcon className={styles.icons} icon={['fab', 'linkedin-in']} />
-                        <span className={styles.text}>Connect with me</span>
-                    </a>
-                </li>
-                <li className={styles.modes}>
-                    <a rel="noopener noreferrer" 
-                    href="https://github.com/crisner" 
-                    target="_blank">
-                        <FontAwesomeIcon className={styles.icons} icon={['fab', 'github']} />
-                        <span className={styles.text}>Find me on Github</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    );
+class Contact extends Component {
+    state = { hover: null }
+
+    onHover = (e) => {
+        this.setState({ hover: e.target.id });
+    }
+
+    onLeave = () => {
+        this.setState({ hover: null });
+    }
+    
+    render() {
+        return (
+            <div className={styles.position}>
+                <h3>Get in touch</h3>
+                <ul className={styles.contact}>
+                    <li className={styles.modes}>
+                        <a
+                        id="0" 
+                        onMouseEnter={this.onHover.bind(this)} 
+                        onMouseLeave={this.onLeave} 
+                        href="mailto:christie.reni@gmail.com">
+                            <FontAwesomeIcon
+                            id="0" 
+                            className={this.state.hover === '0' ? styles.icons + ' ' + styles.iconsOnHover : styles.icons} 
+                            icon="envelope" />
+                            <span id="0" className={styles.text}>Send an email</span>
+                        </a>
+                    </li>
+                    <li className={styles.modes}>
+                        <a
+                        id="1" 
+                        onMouseEnter={this.onHover.bind(this)} 
+                        onMouseLeave={this.onLeave} 
+                        rel="noopener noreferrer" 
+                        href="https://www.linkedin.com/in/renishachristie/" 
+                        target="_blank">
+                            <FontAwesomeIcon 
+                            id="1" 
+                            className={this.state.hover === '1' ? styles.icons + ' ' + styles.iconsOnHover : styles.icons}
+                            icon={['fab', 'linkedin-in']} />
+                            <span id="1" className={styles.text}>Connect with me</span>
+                        </a>
+                    </li>
+                    <li className={styles.modes}>
+                        <a
+                        id="2" 
+                        onMouseEnter={this.onHover.bind(this)} 
+                        onMouseLeave={this.onLeave} 
+                        rel="noopener noreferrer" 
+                        href="https://github.com/crisner" 
+                        target="_blank">
+                            <FontAwesomeIcon 
+                            id="2"
+                            className={this.state.hover === '2' ? styles.icons + ' ' + styles.iconsOnHover : styles.icons} 
+                            icon={['fab', 'github']} />
+                            <span id="2" className={styles.text}>Find me on Github</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default Contact;
