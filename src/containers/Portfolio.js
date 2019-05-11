03dbae2f218f7b3d49826ea5342047a32988627a
crisner <christie.reni@gmail.com>
2019-05-11 21:00:39 +0530

    Correct portfolio spelling

--- src/containers/Portfolio.js
+++ src/containers/Portfolio.js
@@ -22,7 +22,7 @@ class Porfolio extends Component {
     render() {
       return (
         <div className={styles.position}>
-          <h3>Porfolio</h3>
+          <h3>Portfolio</h3>
           <div className={styles.portfolioContainer}>
           {
               this.state.portfolio.map(item => {
